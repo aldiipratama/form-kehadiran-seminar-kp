bc74d0b5c0bf371091c26c62c690adb5bc3bbba1
aldiipratama <paldi0013@gmail.com>
2026-01-01 20:34:48 +0700

    fix: update document title for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Form from "./components/pages/form-kehadiran";
 
-document.title = "Form Kehadiran Seminar kerja Praktek";
-
 function App() {
   return (
     <main className="h-screen w-screen p-5 overflow-x-hidden">
